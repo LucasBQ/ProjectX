2f328f1e9ddeadc13c86992fec8a76b93b54fa06
LucasBQ <10766315+LucasBQ@users.noreply.github.com>
2024-05-31 16:54:12 -0300

    Base Mod

---
+++ src/main/java/io/github/lucasbq/projectx/ProjectX.java
@@ -0,0 +1,8 @@
+package io.github.lucasbq.projectx;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ProjectX.MOD_ID)
+public class ProjectX {
+    public static final String MOD_ID = "projectx";
+}
