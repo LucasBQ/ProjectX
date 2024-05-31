f58b1c8fa2314fe945d62c2340a03f5190d922af
LucasBQ <10766315+LucasBQ@users.noreply.github.com>
2024-05-31 17:32:10 -0300

    Example Item

--- src/main/java/io/github/lucasbq/projectx/ProjectX.java
+++ src/main/java/io/github/lucasbq/projectx/ProjectX.java
@@ -1,8 +1,17 @@
 package io.github.lucasbq.projectx;
 
+import io.github.lucasbq.projectx.init.ItemInit;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(ProjectX.MOD_ID)
 public class ProjectX {
     public static final String MOD_ID = "projectx";
+
+    public ProjectX() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ItemInit.ITEMS.register(bus);
+    }
 }
